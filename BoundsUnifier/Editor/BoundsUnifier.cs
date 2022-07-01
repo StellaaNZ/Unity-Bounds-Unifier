8017fee8b85e30b3d816c7fe74788e42954411dc
StewartNZ <elitestewartnz@gmail.com>
2022-07-02 00:09:05 +1200

    Upload to GitHub

---
+++ BoundsUnifier/Editor/BoundsUnifier.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BoundsUnifier : EditorWindow
+{
+    GameObject parentObject;
+    SkinnedMeshRenderer mainMesh;
+    bool includeInactive = true;
+
+    [MenuItem("Tools/Bounds Unifier")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(BoundsUnifier));
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Bounds Unifier", EditorStyles.boldLabel);
+        parentObject = EditorGUILayout.ObjectField("Parent Object", parentObject, typeof(GameObject), true) as GameObject;
+        mainMesh = EditorGUILayout.ObjectField("Bounds to copy", mainMesh, typeof(SkinnedMeshRenderer), true) as SkinnedMeshRenderer;
+        includeInactive = EditorGUILayout.Toggle("Include Inactive", includeInactive);
+
+        if (GUILayout.Button("GO"))
+        {
+            ChangeBounds();
+        }
+    }
+
+    private void ChangeBounds()
+    {
+        Vector3 mainExtents = mainMesh.localBounds.extents;
+        Vector3 mainCenter = mainMesh.localBounds.center;
+
+        Bounds mainBounds = mainMesh.localBounds;
+
+        SkinnedMeshRenderer[] skinnedMeshes;
+        
+        if (includeInactive)
+            skinnedMeshes = parentObject.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+        else
+            skinnedMeshes = parentObject.GetComponentsInChildren<SkinnedMeshRenderer>(false);
+
+        Debug.Log("Main Extends: (" + mainExtents.x.ToString() + ", " + mainExtents.y.ToString() + ", " + mainExtents.z.ToString() + ")");
+        Debug.Log("Main Center: (" + mainCenter.x.ToString() + ", " + mainCenter.y.ToString() + ", " + mainCenter.z.ToString() + ")");
+
+        foreach (SkinnedMeshRenderer skinnedMesh in skinnedMeshes)
+        {
+            if (skinnedMesh != mainMesh)
+                skinnedMesh.localBounds = mainBounds;
+        }
+
+        Debug.Log("All Enabled Mesh Bounds Changed");
+    }
+}
---
+++ BoundsUnifier/README.txt
@@ -0,0 +1,9 @@
+Found under Tools/BoundsUnifier
+
+You first need to manually set the bounds of one mesh to the full size.
+
+Bounds Unifier sets all the bounds of Skinned Mesh Renderers that are children to the Parent Object to that of the selected Skinned Mesh Renderer.
+
+With include inactive, inactive gameobjects will get their bounds changed as well, with it off they will be excluded
+
+The mesh being copied can be a child of the parent object and will be ignored.
